c3a844b0accfb024e97d569828a930475a120c69
Haendel Ribeiro <haendelribeiro@gmail.com>
2025-11-16 17:35:31 -0300

    Add files via upload

---
+++ Projeto Petshop/src/model/Funcionario.java
@@ -0,0 +1,47 @@
+package model;
+
+public abstract class Funcionario {
+    private String nome;
+    private char sexo;
+    private int idade;
+    private double salario;
+
+    public Funcionario(String nome, char sexo, int idade, double salario) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+}
---
+++ Projeto Petshop/src/model/Medico.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Medico extends Funcionario{
+
+    public Medico(String nome, char sexo, int idade, double salario) {
+        super(nome, sexo, idade, salario);
+    }
+}
