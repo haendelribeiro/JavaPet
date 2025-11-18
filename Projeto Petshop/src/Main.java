8484042410e074ffb360972a3d1171f63e42e331
kenobinho <lukkinha.com13@gmail.com>
2025-11-18 20:54:21 -0300

    teste de cadastro do Pet

--- Projeto Petshop/src/Main.java
+++ Projeto Petshop/src/Main.java
@@ -1,3 +1,25 @@
-public class main (
+import java.util.Scanner;
+import model.Pet;
 
-)
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        Pet p = new Pet();
+
+        //CADASTRO DO PET
+        System.out.println("Qual o nome do Pet?");
+        p.setNome (sc.next() );
+        System.out.println("Qual a Idade do Pet?");
+        p.setIdade (sc.nextInt());
+        System.out.println("Qual a espécie do Pet?");
+        p.setEspecie (sc.next() );
+        System.out.println("Qual o historico de saude do pet? informe caso haja");
+        p.setHsaude (sc.next() );
+        System.out.println("Informe as vacinas tomadas, caso não haja, informe ao sistema");
+        p.setVacina (sc.next() );
+
+        //teste pet
+        System.out.println(p.getNome());
+    }
+}
---
+++ Projeto Petshop/src/model/Pet.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Pet {
+    private String nome;
+    private int idade;
+    private String especie;
+    private String vacina;
+    private String hsaude;
+
+    public Pet() {
+        this.nome = nome;
+        this.idade = idade;
+        this.especie = especie;
+        this.hsaude = hsaude;
+        this.vacina = vacina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getVacina() {
+        return vacina;
+    }
+
+    public void setVacina(String vacina) {
+        this.vacina = vacina;
+    }
+
+    public String getHsaude() {
+        return hsaude;
+    }
+
+    public void setHsaude(String hsaude) {
+        this.hsaude = hsaude;
+    }
+}
